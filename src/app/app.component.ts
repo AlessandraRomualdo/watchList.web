0c95662ad0deb51f69407b4320404c132b13cc36
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2024-10-06 21:18:10 -0300

    feat: criado roteamento inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MovieService } from './services/movie/movie.service';
-import { CardMovieComponent } from './shared/card-movie/card-movie.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardMovieComponent, CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
-
-  public movies: any;
   
   constructor(
-    public movieService: MovieService,
+    
   ) {}
 
   ngOnInit() {
-    this.movieService.getMovies().subscribe((data: any) => {
-      this.movies = data;
-      console.log(this.movies);
-    });
+    
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'homepage',
+    pathMatch: 'full'
+  },
+  {
+    path: 'homepage',
+    loadComponent: () => import('./pages/homepage/homepage.component').then(m => m.HomepageComponent),
+    title: 'Homepage'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
+    title: 'Login'
+  },
+  {
+    path: '**',
+    redirectTo: 'homepage'
+  }
+];
