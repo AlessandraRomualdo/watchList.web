f6dbb0b81b1f1af988872d352862367b16675c0e
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2024-10-06 21:19:18 -0300

    fix: adicionado para o tipo serie

--- src/app/shared/card-movie/card-movie.component.ts
+++ src/app/shared/card-movie/card-movie.component.ts
@@ -1,22 +1,29 @@
 import { Component, Input } from "@angular/core";
 import { Movie } from "../../services/movie/movie.types";
+import { Serie } from "../../services/serie/serie.type";
+import { CommonModule } from "@angular/common";
 
 
 @Component({
     selector: 'app-card-movie',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     template: `
     <div class="flex flex-col justify-center items-center w-[250px] h-[450px] bg-primary rounded-md shadow-lg overflow-hidden text-black hover:text-white hover:bg-background hover:bg-opacity-30 cursor-pointer">
         <img class="w-full h-[370px] object-cover" [src]="movie.poster" alt="{{ movie.title }}">
         
         <div class="w-full h-[80px] flex flex-col justify-between items-center p-4">
             <h2 class="text-sm font-bold">{{ movie.title }}</h2>
-            <p class="text-sm">{{ movie.gender.gender }}</p>
+            <p class="text-sm">{{ movie.gender.gender }}  <span *ngIf="isSerie(movie)"> | {{ movie.seasons }} temporadas</span></p>
         </div>
     `,
 })
 export class CardMovieComponent {
 
-    @Input() movie!: Movie;
+    @Input() movie!: Movie | Serie;
+
+    // Função para verificar se o input é uma série
+  isSerie(movie: Movie | Serie): movie is Serie {
+    return (movie as Serie).seasons !== undefined;
+  }
 }
