140c39eac9542c763a23cbde2bab354723590d30
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2024-10-08 19:03:21 -0300

    feat: add angular meterial

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { authInterceptor } from './app/interceptors/auth-interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])), // Fornece o HttpClient no modo standalone
+    provideHttpClient(withInterceptors([authInterceptor])), provideAnimationsAsync(), // Fornece o HttpClient no modo standalone
   ],
 }).catch((err) => console.error(err));
