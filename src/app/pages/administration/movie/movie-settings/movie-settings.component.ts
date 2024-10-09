794c7b0c4827eddd07f201ab50fb9f13567edcbb
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2024-10-09 16:50:12 -0300

    refactor: refatorado form com campos de pesquisa

--- src/app/pages/administration/movie/movie-settings/movie-settings.component.ts
+++ src/app/pages/administration/movie/movie-settings/movie-settings.component.ts
@@ -8,6 +8,7 @@ import { Gender } from "../../../../services/gender/gender.types";
 import { debounceTime } from "rxjs";
 import { Router } from "@angular/router";
 import { MovieListItemComponent } from "../../components/movie-item-list/movie-item-list.component";
+import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
 
 
 @Component({
@@ -16,64 +17,12 @@ import { MovieListItemComponent } from "../../components/movie-item-list/movie-i
     template: `
         <section class="bg-background w-full flex flex-col  min-h-screen">
         <h1 class="mt-4 text-white text-3xl font-bold border-b-2 border-primary text-center">Gerenciamento de Filmes</h1>
-            <form [formGroup]="form" class="mt-8 w-full flex flex-col md:flex-row items-center justify-between p-2 gap-4">
-                <div class="w-full flex flex-col items-start justify-start">
-                    <label class="py-2 text-white" for="search">Busca</label>
-                    <input
-                        class="h-10 w-full outline-none border text-white border-gray-300 p-2 rounded-xl bg-background-secondary" 
-                        id="search" 
-                        type="text"
-                        formControlName="title"
-                        placeholder="Busque o nome do filme">
-                </div>
-                <div class="w-full flex flex-col items-start justify-start">
-                    <label class="py-2 text-white" for="genero">Genero</label>
-                    <select
-                        id="genero"
-                        formControlName="gender"
-                        class="h-10 w-full border text-white border-gray-300 p-2 rounded-xl bg-background-secondary">
-                        <option value="">Selecione um genero</option>
-                        <option *ngFor="let gender of genders" value={{gender.gender}}>{{ gender.gender }}</option>
-                    </select>
-                </div>
-                <div class="w-full flex items-center justify-center gap-4">
-                    <input 
-                        type="radio" 
-                        name="order" 
-                        id="asc" 
-                        value="asc" 
-                        formControlName="order"
-                        class="hidden"
-                    />
-                    <label 
-                        class="mt-8 text-white font-bold text-sm cursor-pointer p-2 bg-primary border border-primary rounded-lg overflow-hidden hover:bg-primary-hover"
-                         [ngClass]="{ 'bg-primary-dark text-white': form.get('order')?.value === 'asc' }"
-                        for="asc"
-                    >A - Z</label>
-
-                    <input 
-                        type="radio" 
-                        name="order" 
-                        id="desc" 
-                        value="desc" 
-                        formControlName="order"
-                        class="hidden"
-                    />
-                    <label 
-                        class="mt-8 text-white font-bold text-sm cursor-pointer p-2 bg-primary border border-primary rounded-lg overflow-hidden hover:bg-primary-hover"
-                        [ngClass]="{ 'bg-primary-dark text-white': form.get('order')?.value === 'desc' }"
-                        for="desc"
-                    >Z - A</label>
-                    </div>
-
-                <div class="w-full flex justify-center">
-                    <button
-                        type="button"
-                        (click)="newMovie()"
-                        class="mt-9 text-white font-bold bg-primary px-4 py-2 border border-primary rounded-full hover:bg-primary-hover"
-                    > + Novo Filme</button>
-                </div>
-            </form>
+            <app-search-bar 
+                [IsAdmin]="true"
+                [form]="form"
+                titleBtn="+ Novo Filme"
+                (btnClick)="newMovie()"
+            ></app-search-bar>
             <div class="mt-8">
                 <div *ngIf="!noMoviesFound">
                     <app-movie-list-item
@@ -89,12 +38,12 @@ import { MovieListItemComponent } from "../../components/movie-item-list/movie-i
             </div>
         </section>
     `,
-    imports: [MovieListItemComponent, CommonModule, ReactiveFormsModule]
+    imports: [MovieListItemComponent, CommonModule, ReactiveFormsModule, SearchBarComponent],
 })
 export class MovieSettingsComponent implements OnInit{
 
     public movies: Movie[] = [];
-    public genders: Gender[] = [];
+    
     public noMoviesFound: boolean = false;
 
     public form!: FormGroup;
@@ -102,14 +51,13 @@ export class MovieSettingsComponent implements OnInit{
     
     constructor(
         private movieService: MovieService,
-        private genderService: GenderService,
+        
         private router: Router,
     ) {}
 
     ngOnInit(): void {
         this.buidForm();
         this.getMovies();
-        this.getGenders();
 
         // usando o debounceTime para diminuir a quantidade de requisições ao servidor
         this.form.valueChanges.pipe(
@@ -139,11 +87,7 @@ export class MovieSettingsComponent implements OnInit{
           });
     }
 
-    getGenders(): void {
-        this.genderService.getGenders().subscribe((data: any) => {
-            this.genders = data;
-        });    
-    }
+   
 
     buidForm(): void {
         this.form = new FormGroup({
